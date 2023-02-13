810299143cf7444c5044f25b6756532cca28f57c
nithin-ten <pinkyten@sheridancollege.ca>
2023-02-13 11:44:05 -0500

    ICE 1 version 1.11

---
+++ src/ice2/Card.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ice2;
+
+/**
+ *
+ * @author nithi
+ */
+public class Card {
+    
+    /*enum - special class, user defined datatype - constants
+    avangers - card game - 4 enum value safety and type saftey, loose class */
+    
+    
+    public enum Suit {HEARTS,SPADES,CLUBS,DIAMONDS};
+    public enum Value {ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING};
+    private Suit s;
+    private Value v;
+    
+}
